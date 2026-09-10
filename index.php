e70b0365306e0fc57eeb4dbb31614cbd24d1def0
Kowens94 <owenskendall2017@yahoo.com>
2026-09-10 10:33:37 -0400

    New Push
    new push

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Riyah Music Store</title>
+</head>
+<body>
+
+    <div id="app">
+    <iframe src= "./View/online_store.html"></iframe>
+    </div>
+</body>
+</html>
---
+++ js/index.js
@@ -0,0 +1,8 @@
+(function($){
+    'use strict';
+
+    const bootstrap = () => {
+        $.querySelectorAll('img';
+        consolge.log("Images: "))
+    }
+})
